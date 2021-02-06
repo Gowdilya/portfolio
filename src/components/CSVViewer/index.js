feece8652a0b0109cd71a0b3743671a242d7bd8d
Gowdilya Jeyakumar <gowdilyajeyakumar@Gowdilyas-MacBook-Pro.local>
2021-02-06 17:15:07 -0500

    Router Links

---
+++ src/components/CSVViewer/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export default function CSVViewer() {
+    return(<div>
+        CSVViewer
+    </div>)
+}
---
+++ src/components/SidePanel/index.js
@@ -0,0 +1,204 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import HomeIcon from '@material-ui/icons/Home';
+import DescriptionIcon from '@material-ui/icons/Description';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ExtensionIcon from '@material-ui/icons/Extension';
+import TableChartIcon from '@material-ui/icons/TableChart';
+import {Link} from 'react-router-dom';
+import './sidepanel.scss';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+}));
+
+
+export default function SidePanel(props) {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
+  
+    const handleDrawerOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleDrawerClose = () => {
+      setOpen(false);
+    };
+return(
+  <div className="side-panel">
+    <div className={classes.root}>
+    <CssBaseline />
+    <AppBar
+      position="fixed"
+      className={clsx(classes.appBar, {
+        [classes.appBarShift]: open,
+      })}
+    >
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          onClick={handleDrawerOpen}
+          edge="start"
+          className={clsx(classes.menuButton, open && classes.hide)}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" noWrap>
+          Gowdilya's Portfolio
+        </Typography>
+      </Toolbar>
+    </AppBar>
+    <Drawer
+      className={classes.drawer}
+      variant="persistent"
+      anchor="left"
+      open={open}
+      classes={{
+        paper: classes.drawerPaper,
+      }}
+    >
+      <div className={classes.drawerHeader}>
+        <IconButton onClick={handleDrawerClose}>
+          {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+        </IconButton>
+      </div>
+      <Divider />
+
+      <List className="primary-nav">
+
+          <Link to="/">
+            <ListItem button key={"Home"} >
+              <ListItemIcon><HomeIcon></HomeIcon></ListItemIcon>
+              <ListItemText primary={"Home"} />
+            </ListItem>
+          </Link>
+
+          <Link to="/resume">
+            <ListItem button key={"Resume"} >
+              <ListItemIcon><DescriptionIcon></DescriptionIcon></ListItemIcon>
+              <ListItemText primary={"Resume"} />
+            </ListItem>
+          </Link>
+         
+
+      </List>
+
+      <Divider />
+      <List>
+      <Accordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+      >
+        <ListItem button key={"Components"}>
+            <ListItemIcon><ExtensionIcon></ExtensionIcon></ListItemIcon>
+            <ListItemText primary={"Components"} />
+        </ListItem>
+
+        
+  
+      </AccordionSummary>
+      <AccordionDetails>
+        <Link to="csv-viewer">
+          <ListItem button key={"CSV Viewer"} >
+            <ListItemIcon><TableChartIcon></TableChartIcon></ListItemIcon>
+            <ListItemText primary={"CSV Viewer"} />
+          </ListItem>
+        </Link>
+      </AccordionDetails>
+    </Accordion>
+
+
+
+    </List>
+    </Drawer>
+    <main
+      className={clsx(classes.content, {
+        [classes.contentShift]: open,
+      })}
+    >
+      <div className={classes.drawerHeader} />
+     <div>
+       {props.children}
+     </div>
+    </main>
+  </div>
+  </div>
+)
+}
