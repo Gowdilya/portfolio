e7109c673097c3f415a147f87c5a0206214ff921
Gowdilya Jeyakumar <gowdilyajeyakumar@Gowdilyas-MacBook-Pro.local>
2021-02-07 11:30:29 -0500

    Viewer Skeleton Complete

---
+++ src/components/CSVViewer/DataViewer/index.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState, useRef } from "react";
+
+export default function DataViewer(props) {
+    console.log(props.data)
+ return(<div>
+     <table >
+         <thead>
+     <tr>
+         <th> Row #</th>
+    {props.data.headers.map((h, i)=>{
+        return(<th key={i}>{h}</th>)
+    })}
+  </tr>
+  </thead>
+  <tbody>
+      
+  {props.data.lines.map((l, i)=>{
+      return(
+        <tr key={i}>
+            <td>{i}</td>
+            {l.map((col, i)=>{
+                return(
+                    <td key={i}>{col}</td>
+                )
+            })}
+        </tr>
+        )
+    })}
+  </tbody>
+</table>
+ </div>)
+}
--- src/components/CSVViewer/index.js
+++ src/components/CSVViewer/index.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
+import DataViewer from './DataViewer';
+
 // It's not clear to me how to trigger updates to the UI
 const useForceUpdate = () => useState()[1];
 
@@ -7,16 +9,17 @@ const useForceUpdate = () => useState()[1];
 export default function CSVViewer() {
 
     const [file, setFile] = useState(null);
+    const [csvParsed, setcsvParsed] = useState(null);  
 
     useEffect(()=>{
-        console.log(file);
         if(file){
         let reader = new FileReader();
         reader.readAsText(file);
 
         reader.onload = function() {
             //csv content
-            console.log(reader.result);
+
+            processData(reader.result);
           };
 
         reader.onerror = function() {
@@ -25,12 +28,54 @@ export default function CSVViewer() {
         }
 
     }, [file])
+
+    function processData(allText) {
+        var allTextLines = allText.split(/\r\n|\n/);
+        var headers = allTextLines[0].split(',');
+        var csvJSON={}
+        var lines = [];
+    
+        for (var i=1; i<allTextLines.length; i++) {
+            var data = allTextLines[i].split(',');
+            if (data.length == headers.length) {
+    
+                var tarr = [];
+                for (var j=0; j<headers.length; j++) {
+                    tarr.push(data[j]);
+                }
+                lines.push(tarr);
+            }
+        }
+        // alert(lines);
+        csvJSON.headers = headers;
+        csvJSON.lines = lines;
+        setcsvParsed(csvJSON);
+    }
     const onFileChange = event =>{
         setFile(event.target.files[0]);
     }
 
     return(<div>
         <div>
-    <input type="file" accept=".csv" onChange={onFileChange} /></div>
+            Please select a csv such that the first row is the column headers:
+        </div>
+        <div>
+           Download the example template to see the format:
+        </div>
+        <div>
+            <a href='./template.csv' download>Click to Download Template</a>
+        </div>
+        <div>
+           Upload Template to View:
+        </div>
+        <div>
+            <input type="file" accept=".csv" onChange={onFileChange} />
+        </div>
+        <div>
+            {csvParsed?
+            <DataViewer data={csvParsed}>
+
+            </DataViewer>:null}
+        </div>
     </div>)
 }
