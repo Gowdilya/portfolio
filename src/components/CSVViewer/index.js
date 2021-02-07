f1cf16a7b10cc3db74a2e243a3a048d341a45c6a
Gowdilya Jeyakumar <gowdilyajeyakumar@Gowdilyas-MacBook-Pro.local>
2021-02-07 09:57:47 -0500

    console.log csv values

--- src/components/CSVViewer/index.js
+++ src/components/CSVViewer/index.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from "react";
+// It's not clear to me how to trigger updates to the UI
+const useForceUpdate = () => useState()[1];
+
 
 
 export default function CSVViewer() {
+
+    const [file, setFile] = useState(null);
+
+    useEffect(()=>{
+        console.log(file);
+        if(file){
+        let reader = new FileReader();
+        reader.readAsText(file);
+
+        reader.onload = function() {
+            //csv content
+            console.log(reader.result);
+          };
+
+        reader.onerror = function() {
+            console.log(reader.error);
+          };
+        }
+
+    }, [file])
+    const onFileChange = event =>{
+        setFile(event.target.files[0]);
+    }
+
     return(<div>
-        CSVViewer
+        <div>
+    <input type="file" accept=".csv" onChange={onFileChange} /></div>
     </div>)
 }
