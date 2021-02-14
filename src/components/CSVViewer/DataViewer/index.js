ea8410627a7f613a888ed08355b1c461a816f193
Gowdilya Jeyakumar <gowdilyajeyakumar@Gowdilyas-MacBook-Pro.local>
2021-02-14 18:15:48 -0500

    Modified DataViewer to have alphabetical column names

--- src/components/CSVViewer/DataViewer/index.js
+++ src/components/CSVViewer/DataViewer/index.js
@@ -7,9 +7,12 @@ import TextField from '@material-ui/core/TextField';
 export default function DataViewer(props) {
     const [rows, setRows] = useState({ isStartingRow: true, rowIndexes: [0, props.data.lines.length-1] });
 
+    useEffect(()=>{
+        setRows({ isStartingRow: true, rowIndexes: [0, props.data.lines.length-1] });
+    }, [props.data]) // Reset Rows Selected on new Data Set
 
     useEffect(() => {
-        console.log("start: " + rows.rowIndexes[0]);
+        console.log("start: " + rows.rowIndexes[0] );
         console.log("end: " + rows.rowIndexes[1]);
         console.log("IS:"+ rows.isStartingRow);
     }, [rows])
@@ -34,6 +37,33 @@ export default function DataViewer(props) {
         }
     }
 
+    const generateColumnNames = ()=>{
+        var columns = []
+        columns.push(<th></th>)
+        for (let i = 0; i < props.data.headers.length; i ++){
+            columns.push(<th>{getColName(i + 1)}</th>)
+        }
+        return <tr>{ columns }</tr>
+    }
+
+    function getColName(colNum){ // example: 16384 => "XFD"
+        let mostSig = Math.max(0, Math.floor((colNum - 26 - 1)/26**2));
+        let midSig = Math.max(0, Math.floor((colNum - mostSig*26**2 - 1)/26));
+        let leastSig = colNum - mostSig*26**2 - midSig*26;
+
+        return String.fromCharCode(...[mostSig,midSig,leastSig].filter(d=>d).map(d=>d+64));
+    }
+
+    var convertColNametoNum = function(val) {
+        var base = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', i, j, result = 0;
+      
+        for (i = 0, j = val.length - 1; i < val.length; i += 1, j -= 1) {
+          result += Math.pow(base.length, j) * (base.indexOf(val[i]) + 1);
+        }
+      
+        return result;
+    };
+    
 
     return (<div className="dataViewer">
          <TextField
@@ -42,7 +72,7 @@ export default function DataViewer(props) {
           id="standard-number"
           label="Starting Index"
           type="number"
-          value={rows.rowIndexes[0]}
+          value={rows.rowIndexes[0] + 2}
           onChange={(e)=>handleStartChange(e)}
           InputLabelProps={{
             shrink: true,
@@ -54,7 +84,7 @@ export default function DataViewer(props) {
           id="standard-number"
           label="Ending Index"
           type="number"
-          value={rows.rowIndexes[1]}
+          value={rows.rowIndexes[1] + 2}
           onChange={(e)=>handleEndChange(e)}
           InputLabelProps={{
             shrink: true,
@@ -63,19 +93,29 @@ export default function DataViewer(props) {
 
         <table >
             <thead >
-                <tr>
+                {/* <tr>
                     <th> Row #</th>
                     {props.data.headers.map((h, i) => {
                         return (<th key={i}>{h}</th>)
                     })}
-                </tr>
+                </tr> */}
+                
+                    {generateColumnNames()}
+           
             </thead>
             <tbody>
+               
+                <tr>
+                    <td>{1}</td>
+                    {props.data.headers.map((h, i) => {
+                            return (<td key={i}>{h}</td>)
+                        })}
+                </tr>
 
-                {props.data.lines.map((l, rowIndex) => {
+                {props.data.lines.map((l, rowIndex ) => {
                     return (
                         <tr key={rowIndex} onClick={() => handleRowClick(rowIndex)} className={rows.rowIndexes[0] <= rowIndex && rowIndex <= rows.rowIndexes[1] ? "highlight" : ""}>
-                            <td>{rowIndex}</td>
+                            <td>{rowIndex + 2}</td>
                             {l.map((col, j) => {
                                 return (
                                     <td key={j} >{col}</td>
