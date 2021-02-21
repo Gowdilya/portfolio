2b66c34ea970b5ae934e0562351d607768e2e58b
Gowdilya Jeyakumar <gowdilyajeyakumar@Gowdilyas-MacBook-Pro.local>
2021-02-21 17:07:23 -0500

    Update Link

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,7 +7,7 @@ export default function Home() {
         <p>Welcome to my Portfolio Page, hosted directly from my GitHub via GitHub Pages.</p>
         <p> &lt;-- Feel free to explore by opening up the menu on the left corner.</p>
         <p>LinkedIn:
-            <a href={"https://www.linkedin.com/in/gowdilya-jeyakumar/"}> https://github.com/Gowdilya/my_resources </a>
+            <a href={"https://www.linkedin.com/in/gowdilya-jeyakumar/"}> https://www.linkedin.com/in/gowdilya-jeyakumar/ </a>
         </p>
         <p>My Resources:
             <a href={"https://github.com/Gowdilya/my_resources"}> https://github.com/Gowdilya/my_resources </a>
