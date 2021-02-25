3f46a36a9159f3929777f4e4b098edad680428fd
Gowdilya Jeyakumar <gowdilyajeyakumar@Gowdilyas-MacBook-Pro.local>
2021-02-24 22:48:13 -0500

    Refactored

--- src/components/CSVViewer/DataViewer/index.js
+++ src/components/CSVViewer/DataViewer/index.js
@@ -3,19 +3,16 @@ import './dataViewer.scss';
 import TextField from '@material-ui/core/TextField';
 import ErrorMessage from './../../Shared/ErrorMessage';
 
-
-
-
 export default function DataViewer(props) {
     const [coordinates, setCoordinates] = useState({  rowIndexes: [0, props.data.lines.length-1], colIndexes: [1, props.data.lines[0].length] });
     const [coordinateText1, setCoordinateText1] = useState(getColName(coordinates.colIndexes[0])+(coordinates.rowIndexes[0]+1) );
     const [coordinateText2, setCoordinateText2] = useState(getColName(coordinates.colIndexes[1])+(coordinates.rowIndexes[1]+1) );
     const [error1, setError1] = useState({show: false, message:""});
     const [error2, setError2] = useState({show: false, message:""});
+
     useEffect(()=>{
         setCoordinates({  rowIndexes: [0, props.data.lines.length-1] , colIndexes: [1, props.data.lines[0].length]});
     }, [props.data]) // Reset Rows Selected on new Data Set
-
 
 
     useEffect(()=>{
@@ -24,16 +21,9 @@ export default function DataViewer(props) {
         if(!!pattern.test(coordinateText1)){
             setError1({show: false, message:""})
             //valid Text
-            var numb = coordinateText1.replace(/\D/g, "");
-            console.log(numb); // 667000
+            var [row, col] = extractRowAndCol(coordinateText1);
 
-            var char = coordinateText1;
-            char = char.replace(/[0-9]/g, '');
-            console.log(char);
-            var col = convertColNametoNum(char);
-            console.log(col);
-
-            setCoordinates({rowIndexes:[numb-1, coordinates.rowIndexes[1]], colIndexes:[col, coordinates.colIndexes[1]]});
+            setCoordinates({rowIndexes:[row-1, coordinates.rowIndexes[1]], colIndexes:[col, coordinates.colIndexes[1]]});
         }else{
             //invalid Pattern display error
             setError1({show: true, message:"ERROR: Coordinate 1 must be of valid format [Column Charecter][Row Number]"})
@@ -47,37 +37,29 @@ export default function DataViewer(props) {
         if(!!pattern.test(coordinateText2)){
             setError2({show: false, message:""})
             //valid Text
-            var numb = coordinateText2.replace(/\D/g, "");
-            console.log(numb); // 667000
-
-            var char = coordinateText2;
-            char = char.replace(/[0-9]/g, '');
-            console.log(char);
-            var col = convertColNametoNum(char);
-            console.log(col);
-
-            setCoordinates({rowIndexes:[ coordinates.rowIndexes[0],numb-1,], colIndexes:[coordinates.colIndexes[0],col, ]});
+            var [row, col] = extractRowAndCol(coordinateText2);
+            setCoordinates({rowIndexes:[ coordinates.rowIndexes[0],row-1,], colIndexes:[coordinates.colIndexes[0],col, ]});
         }else{
             //invalid Pattern display error
             setError2({show: true, message:"ERROR: Coordinate 2 must be of valid format [Column Charecter][Row Number]"})
         }
-
     },[coordinateText2])
 
-
-
+    const extractRowAndCol  = (coordinate) => {
+        var row = coordinate.replace(/\D/g, "");
+        var char = coordinate;
+        char = char.replace(/[0-9]/g, '');
+        var col = convertColNametoNum(char);
+        return [row, col]
+    }
 
     const handleCoordinate1Change = (event) =>{
-        console.log(event.target.value);
         setCoordinateText1(event.target.value);
     }
 
     const handleCoordinate2Change = (event) =>{
-        console.log(event.target.value);
         setCoordinateText2(event.target.value);
     }
-
-
 
     const generateColumnNames = ()=>{
         var columns = []
@@ -92,7 +74,6 @@ export default function DataViewer(props) {
         let mostSig = Math.max(0, Math.floor((colNum - 26 - 1)/26**2));
         let midSig = Math.max(0, Math.floor((colNum - mostSig*26**2 - 1)/26));
         let leastSig = colNum - mostSig*26**2 - midSig*26;
-
         return String.fromCharCode(...[mostSig,midSig,leastSig].filter(d=>d).map(d=>d+64));
     }
 
@@ -104,7 +85,6 @@ export default function DataViewer(props) {
         }
         return result;
     };
-    
 
     return (<div className="dataViewer">
         <TextField id="outlined-basic1" label="Coordinate 1" variant="outlined" 
@@ -120,7 +100,6 @@ export default function DataViewer(props) {
         <table >
             <thead >
                     {generateColumnNames()}
-           
             </thead>
             <tbody>
                 {props.data.lines.map((l, rowIndex ) => {
